12d64bfd0f328e8b74553840f0ed27d9aa5bced7
Gilson <gilson.costa@dce.ufpb.br>
2020-05-23 16:36:47 -0300

    Criando o UsuarioService e os testes do service e repository

---
+++ src/main/java/com/demo/financas/service/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.demo.financas.service;
+
+import com.demo.financas.model.entity.Usuario;
+
+public interface UsuarioService {
+
+	Usuario autenticar(String email, String senha);
+	
+	Usuario salvarUsuario(Usuario usuario);
+	
+	void validarEmail(String email);
+	
+}
---
+++ src/main/java/com/demo/financas/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,50 @@
+package com.demo.financas.service.impl;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.financas.exception.ErroAutenticacao;
+import com.demo.financas.exception.RegraNegocioException;
+import com.demo.financas.model.entity.Usuario;
+import com.demo.financas.model.repository.UsuarioRepository;
+import com.demo.financas.service.UsuarioService;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@Override
+	public Usuario autenticar(String email, String senha) {
+		Optional<Usuario> usuario = this.repository.findByEmail(email);
+		
+		if(!usuario.isPresent()) {
+			throw new ErroAutenticacao("Usuário não encontrado para o email informado.");
+		}
+		if(!usuario.get().getSenha().equals(senha)) {
+			throw new ErroAutenticacao("Senha inválida.");
+		}
+		return usuario.get();
+	}
+
+	@Override
+	@Transactional
+	public Usuario salvarUsuario(Usuario usuario) {
+		this.validarEmail(usuario.getEmail());
+		return this.repository.save(usuario);
+	}
+
+	@Override
+	public void validarEmail(String email) {
+		boolean existe = this.repository.existsByEmail(email);
+		if(existe) {
+			throw new RegraNegocioException("Já existe um usuário cadastrado com este email.");
+		}
+	}
+
+}

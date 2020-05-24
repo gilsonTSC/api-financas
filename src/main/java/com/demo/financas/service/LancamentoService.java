2972f4cf1651e4b5ac1faea5e48326133e30deef
Gilson <gilson.costa@dce.ufpb.br>
2020-05-24 09:08:35 -0300

    Criando a lançamento service

---
+++ src/main/java/com/demo/financas/service/LancamentoService.java
@@ -0,0 +1,22 @@
+package com.demo.financas.service;
+
+import java.util.List;
+
+import com.demo.financas.model.entity.Lancamento;
+import com.demo.financas.model.enums.StatusLancamento;
+
+public interface LancamentoService {
+
+	Lancamento salvar(Lancamento lancamento);
+	
+	Lancamento atualizar(Lancamento lancamento);
+	
+	void deletar(Lancamento lancamento);
+	
+	List<Lancamento> buscar( Lancamento lancamentoFiltro );
+	
+	void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+	
+	void validar(Lancamento lancamento);
+	
+}

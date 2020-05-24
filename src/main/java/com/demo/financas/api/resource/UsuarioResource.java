42df69f5e621e3c0adbcea405eccadb8362feee1
Gilson <gilson.costa@dce.ufpb.br>
2020-05-24 14:17:59 -0300

    Criando o endpoint para obter o saldo

--- src/main/java/com/demo/financas/api/resource/UsuarioResource.java
+++ src/main/java/com/demo/financas/api/resource/UsuarioResource.java
@@ -1,7 +1,12 @@
 package com.demo.financas.api.resource;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,7 @@ import com.demo.financas.api.dto.UsuarioDTO;
 import com.demo.financas.exception.ErroAutenticacao;
 import com.demo.financas.exception.RegraNegocioException;
 import com.demo.financas.model.entity.Usuario;
+import com.demo.financas.service.LancamentoService;
 import com.demo.financas.service.UsuarioService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioResource {
 
 	private final UsuarioService service;
+	private final LancamentoService lancamentoService;
 	
 	@PostMapping("/autenticar")
 	public ResponseEntity<Object> autenticar( @RequestBody UsuarioDTO dto ) {
@@ -47,6 +54,18 @@ public class UsuarioResource {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 		
+	}
+	
+	@GetMapping("{id}/saldo")
+	public ResponseEntity<BigDecimal> obterSaldo( @PathVariable("id") Long id ) {
+		Optional<Usuario> usuario = service.obterPorId(id);
+		
+		if(!usuario.isPresent()) {
+			return new ResponseEntity<BigDecimal>( HttpStatus.NOT_FOUND );
+		}
+		
+		BigDecimal saldo = lancamentoService.obterSaldoPorUsuario(id);
+		return ResponseEntity.ok(saldo);
 	}
 	
 }
